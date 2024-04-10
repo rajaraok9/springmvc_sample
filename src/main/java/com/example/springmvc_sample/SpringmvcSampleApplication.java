9e8da0d1c597351f234b3175640602af5f678f86
trassinfotek <rajaraok9@gmail.com>
2024-04-10 15:57:44 +0530

    SpringmvcSampleApplication

---
+++ src/main/java/com/example/springmvc_sample/SpringmvcSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springmvc_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmvcSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmvcSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springmvc_sample/SpringmvcSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springmvc_sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmvcSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
